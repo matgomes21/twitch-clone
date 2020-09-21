2221a8541f8056fe35ed8ea3ac0f6744b7a0b58f
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2020-09-21 00:05:45 -0300

    feat: Created Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Feather from 'react-native-vector-icons/Feather';
+import colors from '../../styles/colors';
+
+import { Container, Avatar, OnlineStatus, RightSide, Button } from './styles';
+
+const Header: React.FC = () => {
+  return (
+    <Container>
+      <Avatar>
+        <OnlineStatus />
+      </Avatar>
+
+      <RightSide>
+        <Button>
+          <MaterialIcons
+            name="notifications-none"
+            size={26}
+            color={colors.black}
+          />
+        </Button>
+
+        <Button>
+          <MaterialCommunityIcons
+            name="message-outline"
+            size={26}
+            color={colors.black}
+          />
+        </Button>
+
+        <Button>
+          <Feather name="search" size={26} color={colors.black} />
+        </Button>
+      </RightSide>
+    </Container>
+  );
+};
+
+export default Header;
