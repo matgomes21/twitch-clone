2e1cb56e4f01e35fbd31a695f5fa8c57c69eaecc
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2020-10-05 23:35:44 -0300

    feat: Created Following page with components

--- src/pages/Following/styles.ts
+++ src/pages/Following/styles.ts
@@ -1,5 +1,12 @@
 import styled from 'styled-components/native';
+import colors from '../../styles/colors';
 
-export const Container = styled.View`
-
+export const Wrapper = styled.View`
+  background: ${colors.primary};
+  flex: 1;
 `;
+export const Container = styled.View`
+  padding-left: 14px;
+`;
+
+export const Main = styled.View``;
